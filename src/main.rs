cdae373f6d98b8ffd86c09cc5cedccfe28d2f8d8
Ash Logan <ash@heyquark.com>
2021-08-16 22:32:05 +1000

    example: First pass at some OSScreen/app lifecycle action

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,44 @@
 #![feature(start)]
 
 use cafeos as _;
+use cafeos::screen::OSScreen;
+
+unsafe extern "C" fn save_callback() {
+    cafeos_sys::OSSavesDone_ReadyToRelease();
+}
 
 #[start]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
     unsafe {
-        if cafeos_sys::OSIsDebuggerPresent() != 0 {
-            1
-        } else {
-            0
-        }
+        cafeos_sys::ProcUIInit(Some(save_callback));
+        cafeos_sys::OSReport("owo\0".as_ptr() as *const cty::c_char);
     }
+
+    let screen = OSScreen::init().unwrap();
+
+    loop {
+        unsafe {
+            let status = cafeos_sys::ProcUIProcessMessages(1);
+            match status {
+                cafeos_sys::ProcUIStatus::PROCUI_STATUS_IN_FOREGROUND => {}
+                cafeos_sys::ProcUIStatus::PROCUI_STATUS_RELEASE_FOREGROUND => {
+                    cafeos_sys::ProcUIDrawDoneRelease();
+                    continue;
+                }
+                cafeos_sys::ProcUIStatus::PROCUI_STATUS_EXITING => { break; }
+                _ => { continue; }
+            }
+        }
+
+        screen.text("hello world!", 0, 0);
+        screen.flip();
+    }
+
+
+    unsafe {
+        cafeos_sys::ProcUIShutdown();
+        //cafeos_sys::OSFatal("oh dear\0".as_ptr() as *const cty::c_char);
+    }
+
+    0
 }
