2a067e98cb12e513178a71c9593d520635d57442
Ash Logan <ash@heyquark.com>
2021-11-27 11:05:58 +1100

    target: Remove a bit more devkitPPC stuff
    we're taking it slow

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let dkp_path = std::env::var("DEVKITPRO").unwrap();
-    let wut_path = match std::env::var("WUT_ROOT") {
-        Ok(val) => val,
-        Err(_) => format!("{}/wut", dkp_path)
-    };
-    println!("cargo:rustc-link-search=native={}/lib", wut_path);
-    println!("cargo:rustc-link-search=native={}/share", wut_path);
-}
