cb878cabf5dd7ff5b0ae0f4d3ba2c28addc69c42
Ash Logan <ash@heyquark.com>
2021-11-29 22:33:56 +1100

    cafeos-sys: Generate and link rpl imports

--- cafeos-sys/build.rs
+++ cafeos-sys/build.rs
@@ -3,10 +3,29 @@ extern crate bindgen;
 use std::fs;
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
-    //TODO rplimportgen
-    //TODO link rpls
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let cafe_dir = fs::read_dir("ext/wut/cafe").expect("Could not find wut submodule");
+    let asm_files = cafe_dir.map(|rpl| {
+        let rpl_path = rpl.unwrap().path();
+        let asm_path = out_path.join(rpl_path.file_name().unwrap()).with_extension("S");
+
+        Command::new("/opt/devkitpro/tools/bin/rplimportgen")
+            .arg(&rpl_path)
+            .arg(&asm_path)
+            .status()
+            .expect("Could not convert rpl!");
+
+        return asm_path;
+    });
+
+    cc::Build::new()
+        .files(asm_files)
+        .compiler("clang") //could also use powerpc-eabi-gcc from devkitPPC
+        .compile("cafe");
 
     println!("cargo:rerun-if-changed=src/ffi/cafe.h");
 
@@ -35,7 +54,6 @@ fn main() {
         .generate()
         .expect("Unable to generate Cafe bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings.write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write out bindings!");
 }
